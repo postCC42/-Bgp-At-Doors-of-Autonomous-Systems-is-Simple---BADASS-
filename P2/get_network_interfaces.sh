09dd84572127edd938a2493f439ead85b60f1825
bl000m <mathiapagani@gmail.com>
2024-06-14 10:00:15 +0200

    update scripts and added check network

--- P2/get_network_interfaces.sh
+++ P2/get_network_interfaces.sh
@@ -7,23 +7,18 @@ NC='\033[0m' # No Color
 # Get the list of running Docker container IDs
 running_containers=$(docker ps -q)
 
+# Check if there are any running containers
 if [[ ! -z $running_containers ]]; then
   for container_id in ${running_containers[@]}; do
     # Get the hostname of the container
     hostname=$(docker exec $container_id hostname)
+    
     echo "Container ID: $container_id"
     echo -e "${YELLOW}Hostname: $hostname${NC}"
+    echo "Network Interfaces and IP Addresses:"
     
-    # Retrieve network interfaces and MAC addresses
-    docker exec $container_id sh -c "ip link show | awk '/^[0-9]+: / {iface=\$2} /ether/ {print iface, \$2}'"
-    
-    # Output the MAC address table if the bridge br0 exists
-    if docker exec $container_id sh -c "brctl showmacs br0" &> /dev/null; then
-      echo "MAC address table for br0:"
-      docker exec $container_id sh -c "brctl showmacs br0"
-    else
-      echo "No bridge br0 found in this container"
-    fi
+    # Display the full output of `ip a` for the container
+    docker exec $container_id ip a
     
     echo "---------------------------"
   done
